09e35ed1f620e1e095189e2214afb72a0985567c
VladMironchuk <vlad.mironchuk.02@gmail.com>
2021-01-16 19:49:21 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+      for(let i=0;i<bracketsConfig.length+1;i++){
+        for(conf of bracketsConfig){
+            let br = conf[0]+conf[1]
+            while(~str.indexOf(br)){
+                if(str.includes(br)){
+                    str = str.slice(0,str.indexOf(br)) + str.slice(str.indexOf(br)+2)
+                }
+            }
+        }
+    }
+    return str ? false : true
 }
